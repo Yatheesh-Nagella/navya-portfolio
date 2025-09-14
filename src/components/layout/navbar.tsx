950378e2a574bdfc5361af65a5213e175ce4289d
Yatheesh Nagella <yatheeshnagella17@gmail.com>
2025-09-14 17:06:29 -0500

    navbar, Grid in home

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -67,7 +67,7 @@ const Navbar = () => {
             e.currentTarget.style.transform = 'scale(1)';
           }}
         >
-          Navya Nanduri
+          NN
         </button>
 
         {/* Navigation Links */}
